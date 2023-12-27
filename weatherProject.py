e1d9be23a2e2cea136c49653285d40b86aac06f1
Natalie Parker <nataliep0817@gmail.com>
2023-12-27 14:55:28 -0800

    Implemented user input to recieve chosen city's weather

--- weatherProject.py
+++ weatherProject.py
@@ -1,12 +1,23 @@
 import requests
 
 baseUrl = "http://api.weatherapi.com/v1"
-testUrl = "http://api.weatherapi.com/v1/current.json?key=c69cac9f370144a4baa212601232712&q=92071"
-request = requests.get(testUrl);
+request = None;
+data = None;
+
+getInput = input("Enter a city name. \n");
+while True:
+    getUrl = "http://api.weatherapi.com/v1/current.json?key=c69cac9f370144a4baa212601232712&q=" + getInput;
+    request = requests.get(getUrl);
+    data = request.json();
+    if ('error' in data):
+        print("Invalid city name, please try again.");
+        getInput = input();
+    else:
+        break;
+
 
 if (request.status_code == 200):
-    data = request.json();
-    print("RESPONSE: ", data);
+    # print("RESPONSE: ", data);
     print("CITY NAME:", data.get('location', {}).get('name', 'N/A'));
     print("WEATHER:", data.get('current', {}).get('condition', {}).get('text', 'N/A'));
     print("FARENHEIT:", data.get('current', {}).get('temp_f', 'N/A'));
