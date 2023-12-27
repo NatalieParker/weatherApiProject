95ba3fe142ec812d742e53d9e1af98be81cc15cf
Natalie Parker <nataliep0817@gmail.com>
2023-12-27 15:19:48 -0800

    Changed request from getting current day weather to a forecast for one day. Also added min and max farenheit/celcius

--- weatherProject.py
+++ weatherProject.py
@@ -6,7 +6,7 @@ data = None;
 
 getInput = input("Enter a city name. \n");
 while True:
-    getUrl = "http://api.weatherapi.com/v1/current.json?key=c69cac9f370144a4baa212601232712&q=" + getInput;
+    getUrl = "http://api.weatherapi.com/v1/forecast.json?key=c69cac9f370144a4baa212601232712&q=" + getInput;
     request = requests.get(getUrl);
     data = request.json();
     if ('error' in data):
@@ -20,8 +20,10 @@ if (request.status_code == 200):
     # print("RESPONSE: ", data);
     print("CITY NAME:", data.get('location', {}).get('name', 'N/A'));
     print("WEATHER:", data.get('current', {}).get('condition', {}).get('text', 'N/A'));
-    print("FARENHEIT:", data.get('current', {}).get('temp_f', 'N/A'));
-    print("CELCIUS:", data.get('current', {}).get('temp_c', 'N/A'));
+    print("MIN FARENHEIT:", str(data.get('forecast', {}).get("forecastday", [{}])[0].get('day', {}).get('mintemp_f', 'N/A')) +
+          ", MAX FARENHEIT:", str(data.get('forecast', {}).get("forecastday", [{}])[0].get('day', {}).get('maxtemp_f', 'N/A')));
+    print("MIN CELCIUS:", str(data.get('forecast', {}).get("forecastday", [{}])[0].get('day', {}).get('mintemp_c', 'N/A')) +
+          ", MAX CELCIUS: ", str(data.get('forecast', {}).get("forecastday", [{}])[0].get('day', {}).get('maxtemp_c', 'N/A')));
 else:
     print("ERROR: ", request.status_code, request.text);
 
