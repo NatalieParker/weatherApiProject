4102ac992e5d587510620e5907c36d7645d42b68
Natalie Parker <nataliep0817@gmail.com>
2023-12-27 15:36:16 -0800

    Added forecast range to see the weather for a custom amount of days.

--- weatherProject.py
+++ weatherProject.py
@@ -5,8 +5,10 @@ request = None;
 data = None;
 
 getInput = input("Enter a city name. \n");
+forecastRange = str(10);
+
 while True:
-    getUrl = "http://api.weatherapi.com/v1/forecast.json?key=c69cac9f370144a4baa212601232712&q=" + getInput;
+    getUrl = "http://api.weatherapi.com/v1/forecast.json?key=c69cac9f370144a4baa212601232712&q=" + getInput + "&days=" + (forecastRange);
     request = requests.get(getUrl);
     data = request.json();
     if ('error' in data):
@@ -18,12 +20,21 @@ while True:
 
 if (request.status_code == 200):
     # print("RESPONSE: ", data);
-    print("CITY NAME:", data.get('location', {}).get('name', 'N/A'));
-    print("WEATHER:", data.get('current', {}).get('condition', {}).get('text', 'N/A'));
-    print("MIN FARENHEIT:", str(data.get('forecast', {}).get("forecastday", [{}])[0].get('day', {}).get('mintemp_f', 'N/A')) +
-          ", MAX FARENHEIT:", str(data.get('forecast', {}).get("forecastday", [{}])[0].get('day', {}).get('maxtemp_f', 'N/A')));
-    print("MIN CELCIUS:", str(data.get('forecast', {}).get("forecastday", [{}])[0].get('day', {}).get('mintemp_c', 'N/A')) +
-          ", MAX CELCIUS: ", str(data.get('forecast', {}).get("forecastday", [{}])[0].get('day', {}).get('maxtemp_c', 'N/A')));
+    print(forecastRange + "-Day Forecast for:", data.get('location', {}).get('name', 'N/A'));
+    forecast = data.get('forecast', {}).get("forecastday", [{}]);
+    day = 0
+    for date in forecast:
+        print("DATE:", data.get('forecast', {}).get("forecastday", [{}])[day].get('date', 'N/A'));
+        print("WEATHER:", data.get('current', {}).get('condition', {}).get('text', 'N/A'));
+        print("MIN FARENHEIT:", str(data.get('forecast', {}).get("forecastday", [{}])[day].get('day', {}).get('mintemp_f', 'N/A')) +
+            ", MAX FARENHEIT:", str(data.get('forecast', {}).get("forecastday", [{}])[day].get('day', {}).get('maxtemp_f', 'N/A')));
+        print("MIN CELCIUS:", str(data.get('forecast', {}).get("forecastday", [{}])[day].get('day', {}).get('mintemp_c', 'N/A')) +
+            ", MAX CELCIUS: ", str(data.get('forecast', {}).get("forecastday", [{}])[day].get('day', {}).get('maxtemp_c', 'N/A')));
+        print();
+        day+=1;
+
+
+    
 else:
     print("ERROR: ", request.status_code, request.text);
 
